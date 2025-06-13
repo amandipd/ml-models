aef878356d24edad55f39950d849c32685d05d57
Amandip Dutta <amandip@berkeley.edu>
2025-06-13 15:35:54 -0700

    add tran file for lin reg directory

--- Linear Regression/train.py
+++ Linear Regression/train.py
@@ -1,4 +1,19 @@
+import numpy as np
+import pandas as pd
 from LinearRegression import LinearRegression
 
 
+data = pd.read_csv('data.csv')
+X = data.iloc[:, :-1].values 
+y = data.iloc[:, -1].values
 
+# Create and train the model
+model = LinearRegression(lr=0.01, n_iters=1000)
+model.fit(X, y)
+
+
+y_pred = model.predict(X)
+
+# Print the learned parameters
+print("Learned weights:", model.weights)
+print("Learned bias:", model.bias)
